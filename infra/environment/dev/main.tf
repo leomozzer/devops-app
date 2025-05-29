68ae90e385b6bb67d720eb9c37e4c240d7f3eea2
leomozzer <mozzer.ferraz@gmail.com>
2025-05-29 15:39:38 -0300

    Fixed count

--- infra/environment/dev/main.tf
+++ infra/environment/dev/main.tf
@@ -1,14 +1,14 @@
 module "resource_group" {
   source = "../../modules/resource-group"
   name   = "backend"
-  prefix = "002"
+  #prefix = "002"
 }
 
 module "backend_app_service" {
   source              = "../../modules/linux-web-app"
   app_name            = "backend"
   resource_group_name = module.resource_group.output.name
-  prefix              = "002"
+  #prefix              = "002"
 }
 
 output "app_service" {
